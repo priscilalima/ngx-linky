6bb30c5ef3eab933d811043f9b13bf6550095b8b
Rokas Brazdžionis <rokas.brazdzionis@gmail.com>
2016-03-09 18:00:30 +0200

    Implement karma runner

--- karma.conf.js
+++ karma.conf.js
@@ -1,17 +1,27 @@
 module.exports = function (config) {
   config.set({
-    basePath: '',
+    basePath: '.',
 
     frameworks: ['jasmine'],
 
     files: [
-      { pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', included: true, watched: true },
+      // paths loaded by Karma
+      {pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', included: true, watched: true},
+      {pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
+      {pattern: 'node_modules/rxjs/bundles/Rx.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/angular2.dev.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/testing.dev.js', included: true, watched: true},
+      {pattern: 'karma-test-shim.js', included: true, watched: true},
 
-      { pattern: 'src/**/*.js', included: false, watched: true },
-      // 'node_modules/phantomjs-polyfill/bind-polyfill.js',
-      // 'node_modules/es6-shim/es6-shim.js',
-      // 'src/*.js'
+      // paths loaded via module imports
+      {pattern: 'dist/**/*.js', included: false, watched: true},
     ],
+
+    // proxied base paths
+    proxies: {
+        // required for component assests fetched by Angular's compiler
+        '/src/': '/base/src/'
+    },
 
     reporters: ['progress'],
 
